c4a3fe7c3f4ea8e4f26e9b7477bca788f1225de1
Eduardo Xavier <eduardoxavierrf@gmail.com>
2022-01-26 15:43:30 -0300

    refact

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
 from socket import (AF_INET, SOCK_STREAM, socket)
 
+class Request:
+    def __init__(self, request):
+        request = request.decode("utf-8")
+        request = request.split('|')
+        self.role = request[0]
+        self.studentName = request[1]
+        self.action = request[2]
+        self.arg = request[3] if 3 < len(request) else None
+
 grades = {}
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -9,34 +18,32 @@ print('Server started on port 8080 🚀')
 while True:
     connectionSocket, addr = serverSocket.accept()
 
-    request = connectionSocket.recv(1024)
-    
-    request = request.decode("utf-8")
-    request = request.split('|')
-    if (request[0] == 'professor'):
-        if request[2] == 'baixar trabalho':
-            file = open('trabalho_' + request[1] + '.pdf', 'rb')
+    request = Request(connectionSocket.recv(1024))
+
+    if (request.role == 'professor'):
+        if request.action == 'baixar trabalho':
+            file = open('trabalho_' + request.studentName + '.pdf', 'rb')
             file_bytes = file.read(1024)
 
             while file_bytes:
                 connectionSocket.send(file_bytes)
                 file_bytes = file.read(1024)
             file.close()
-        elif request[2] == 'enviar nota':
-            grades[request[1]] = request[3]
+        elif request.action == 'enviar nota':
+            grades[request.studentName] = request.arg
 
-    elif (request[0] == 'aluno'):
-        if request[2] == 'send file':
-            f = open('trabalho_' + request[1] + '.pdf','wb')
+    elif (request.role == 'aluno'):
+        if request.action == 'send file':
+            f = open('trabalho_' + request.studentName + '.pdf','wb')
             file_bytes = connectionSocket.recv(1024)
             while file_bytes:
                 f.write(file_bytes)
                 file_bytes = connectionSocket.recv(1024)
             f.close()
-            
-        elif request[2] == 'consultar nota':
-            if grades.get(request[1]) == None:
+
+        elif request.action == 'consultar nota':
+            if grades.get(request.studentName) == None:
                 connectionSocket.send('Sem nota disponivel'.encode())
             else:
-                connectionSocket.send(grades.get(request[1]).encode())
+                connectionSocket.send(grades.get(request.studentName).encode())
     connectionSocket.close()
